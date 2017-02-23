1003fc5092602720641403a9ac5a0800a2b6bf04
Sven <apocalyps@mostly-useless.net>
2017-02-23 21:37:34 +0100

    build

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,13 @@
 $(function() {
 
   // your code will go here
-
+  $.ajax({
+    url: 'https://www.codeschool.com/users/2248257.json',
+    dataType: 'jsonp',
+    success: function(response) {
+      $.each(response.courses.completed, function( k, v) {
+        $('#badges').append('<div class="course"><h3>' + v.title + '</h3><img src="' + v.badge + '"><a href="' + v.url + '" target="_blank" class="btn btn-primary">See Course</a></div>');
+      });
+    }
+  });
 });
